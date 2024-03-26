dd8c22667c9d619b34531aea40b1bcb1f9a3a628
Nedim Kulasin <nedim.kulasin@teneo.ba>
2024-03-26 15:15:51 +0100

    mail facades

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Product; 
 use App\Models\Order; 
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\OrderPlaced;
 
 class CartController extends Controller
